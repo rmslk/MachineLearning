fa1957a85914a99a8096800215841556d1d28009
rmslk <102646325+rmslk@users.noreply.github.com>
2022-04-12 22:33:53 +0300

    Тренировка и прогнозирование

--- SimpleLearning.py
+++ SimpleLearning.py
@@ -19,3 +19,15 @@ for i in range(train_set_count):
 
 for i in range(20):
     print(train_input[i], train_output[i])
+
+# Тренировка
+predictor = LinearRegression()
+predictor.fit(X=train_input, y=train_output) 
+
+# Прогнозирование
+x_test = [[10, 20, 30]]
+outcome = predictor.predict(X=x_test)
+coefficients = predictor.coef_
+
+print('Outcome : ', outcome)
+print('Coefficients : ', coefficients)
