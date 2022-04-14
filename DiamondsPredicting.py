966dc88406803d2bd106a3d98d19425d37d7cac4
rmslk <termitdream@gmail.com>
2022-04-14 12:27:08 +0300

    Построение графиков

--- DiamondsPredicting.py
+++ DiamondsPredicting.py
@@ -42,3 +42,15 @@ result['price'] = y_test
 result['prediction'] = predictions.tolist()
 
 print(result.to_string())
+
+# Определение оси Х
+x_axis = X_test.carat
+
+# Построение графиков
+plt.scatter(x_axis, y_test, c = 'b', alpha = 0.5, marker = '.', label = 'Real')
+plt.scatter(x_axis, predictions, c = 'r', alpha = 0.5, marker = '.', label = 'Predicted')
+plt.xlabel('Carat')
+plt.ylabel('Price')
+plt.grid(color = '#D3D3D3', linestyle = 'solid')
+plt.legend(loc = 'lower right')
+plt.show()
